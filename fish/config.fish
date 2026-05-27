7d9dc691466c1579eaa6d8379180ff964b81e6d8
duckuments <duckuments@gmail.com>
2026-05-27 21:46:36 +0330

    feat(fish): 🎸 update theme, added some alias and more tools

--- fish/config.fish
+++ fish/config.fish
@@ -4,29 +4,29 @@ if status is-interactive
     # fastfetch | lolcat
     # set -U fish_greeting
 end
-
 # Set up Homebrew environment
 set -gx HOMEBREW_PREFIX /home/linuxbrew/.linuxbrew
 set -gx HOMEBREW_CELLAR $HOMEBREW_PREFIX/Cellar
 set -gx HOMEBREW_REPOSITORY $HOMEBREW_PREFIX/Homebrew
-
 # Add brew to PATH
 set -gx PATH $HOMEBREW_PREFIX/bin $HOMEBREW_PREFIX/sbin $PATH
-
 # Optional: add man and info paths
 set -gx MANPATH $HOMEBREW_PREFIX/share/man $MANPATH
 set -gx INFOPATH $HOMEBREW_PREFIX/share/info $INFOPATH
-
 alias vim='nvim'
 alias ll='ls -asl'
 alias cls='clear'
 alias g='git'
 alias lg='lazygit'
+alias tok='bun run tokscale'
+alias ei='yazi'
+alias o='bat'
 
-# Add to ~/.bashrc or ~/.zshrc file
-export ANTHROPIC_BASE_URL=https://agentrouter.org/
-export ANTHROPIC_AUTH_TOKEN=WD5JDousrATtT/svRJOFeafSwXLJe7Fg
-export ANTHROPIC_API_KEY=sk-JJQ30ATZhD5x2ARQdnRgbQ86QwbVFb44xt78F2PF2XuZpVjg
+alias gs='git status'
+alias gl='git log'
+alias dev='pnpm dev'
+alias build='pnpm build'
+alias lint='pnpm lint'
 
 # set proxy for shell 
 set -x http_proxy http://localhost:10808
@@ -34,10 +34,24 @@ set -x https_proxy http://localhost:10808
 set -x all_proxy socks5://localhost:10808
 
 # open file from fzf with neovim
-
 function fj
-    set file (fzf --preview='cat {}')
+    set file (fzf --preview='bat {}')
     if test -n "$file"
         nvim "$file"
     end
 end
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
+
+# # nvm-fish integration - added automatically
+# # You must call it on initialization or directory switching won't work
+# load_nvm > /dev/stderr
+
+# set editor to nvim
+set -x EDITOR nvim
+set -x VISUAL nvim
+
+# config Homebrew
+set -gx HOMEBREW_PREFIX /home/linuxbrew/.linuxbrew
+set -gx PATH $HOMEBREW_PREFIX/bin $HOMEBREW_PREFIX/sbin $PATH
